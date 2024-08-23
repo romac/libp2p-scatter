8b449e0b2a4f99c084841de7782845efebeba686
Romain Ruetschi <romain@informal.systems>
2024-08-23 09:44:08 +0200

    Update to libp2p v0.54.x

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,7 @@ impl NetworkBehaviour for Broadcast {
         _peer: PeerId,
         _addr: &Multiaddr,
         _role_override: libp2p::core::Endpoint,
+        _port_use: libp2p::core::transport::PortUse,
     ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
         Ok(Handler::default())
     }
